007dec017f483eac84e29ef085e8693747b3d94f
Katana-Development <katana.development@gmail.com>
2020-11-07 19:26:45 -0800

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+
+/** 
+*  @prettier 
+*  @name PlainTextInterface
+*  @license MIT
+*  @file "./index.js" 
+*  @author Andrew J Chambers
+*  @fileoverview Main file for the root project, 'PlainTextInterface'.
+**/ 
+
+'strict';
+
+
+
