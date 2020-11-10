7203f198dda8bae6b0682605ec3b1471d7e4b847
Katana-Development <katana.development@gmail.com>
2020-11-10 11:32:35 -0800

    [SUBJECT: 'class TextDocument'] -- 'Msg: Finished constructor for class: TextDocument. Class object creates and/or opens file and reads content if any. Created getter & setter for files text'

--- bin/ptcli.js
+++ bin/ptcli.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
+/** @format */
 
 const chalk = require('chalk');
 const command = process.argv;
-const {MSG_INITITAL} = require('./lib/constants.json').CLI;
+const { MSG_INITITAL } = require('../lib/constants.json').CLI;
 
 console.info(MSG_INITITAL);
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/** @format */
+
+3;
 /**
  *  @prettier
  *  @name PlainTextInterface
@@ -9,48 +12,120 @@
  *  @fileoverview
  **/
 
-'strict';
-
-/**
- *  @prettier
- *  @name
- *  @license MIT
- *  @public
- *  @summary https://github.com/Katana-Development/.git
- *  @author Andrew J Chambers
- *  @file"./"
- *  @fileoverview
- **/
-
-'strict';
+('strict');
 
 const fs = require('fs');
-const {O_CREAT, O_RDWR} = fs.constants;
+const { O_CREAT, O_RDWR } = fs.constants;
+const { Buffer } = require('buffer');
+const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
+const { runInThisContext } = require('vm');
 
-class PlainTextDocument {
-    constructor(filepath, maxMemorySz = 880000, fperms = 775) {
-        const buf = Buffer.alloc(maxMemorySz);
-        const fd = fs.openSync(filepath, O_CREAT, O_RDWR);
+/**
+ * @name TextDocument
+ * @classdesc Creates an abstraction of a plain-text file, and implements an interface to handle the files data, handle the file as data and to alter or manipulate the file.
+ **/
+class TextDocument {
+    //<|| PUBLIC ||>
+    filepath = '/';
+    textSz = 0;
+    size = 0;
 
-        fs.chmodSync(filepath, fperms);
-        fs.readSync(fd, buf);
+    //<|| PRIVATE ||>
+    #fd = 0;
+    #permissions = '';
+    #stats = {};
+    stat = {};
+    #text = '';
+    #maxTextSz = '';
+    #buff;
 
+    /**
+     * @constructs TextDocument
+     * @param {import('fs').PathLike} filepath
+     * @param {number} maxBytes
+     * @param {boolean} safeAlgorithm
+     */
+    constructor(
+        filepath = '',
+        opts = { permissions: 0o664, addMemory: 1000000, safeBuf: true }
+    ) {
+        // |> Save Parameterz
         this.filepath = filepath;
-        this.memSz = maxMemorySz;
-        this.stat = fs.statSync(filepath);
-        this._text = '';
-        this.text = buf.toString('utf-8');
+        this.#permissions = opts.permissions;
 
-        fs.closeSync(fd);
+        // |> Opening &/or Creating File then setting file permissions.
+        this.#fd = fs.openSync(filepath, O_CREAT | O_RDWR);
+        fs.chmodSync(filepath, this.#permissions);
+
+        // |> Handling Memory task, creating Mem-Info access for API users to print.
+        this.#stats = fs.statSync(this.filepath);
+        this.textSz = this.#stats.size;
+        this.#maxTextSz = this.textSz + opts.addMemory;
+
+        // |> Allocating Memory.
+        if (opts.safeBuf) this.#buff = Buffer.alloc(this.#maxTextSz);
+        else this.#buff = Buffer.allocUnsafe(this.#maxTextSz);
+
+        // |> Reading file, then encoding bianary into UTF-8-Chars.
+        fs.readSync(this.#fd, this.#buff);
+        this.text = this.#buff.toString('utf8', 0, this.textSz);
+
+        // |> Closing File.
+        fs.closeSync(this.#fd);
+    }
+
+    get maxTextSz() {
+        return this.#maxTextSz;
     }
 
     get text() {
-        return this._text;
+        return this.#text;
     }
 
     set text(text) {
-        this._text = text;
+        this.#text = text;
+    }
+
+    setPermissions() {
+        return 0;
+    }
+
+    save() {
+        return 0;
+    }
+
+    setLineLength(maxCharsPerLine = 80) {
+        // ==> CONSTANTS
+        const text = this.text;
+        const lineLen = maxCharsPerLine;
+
+        // >-> VARIABLES
+        let altText = '';
+        let charCount = lineLen;
+        let prevCount = 0;
+        let char = '';
+        let line = '';
+
+        while (charCount < text.length) {
+            while (char != ' ') {
+                char = text.charAt(charCount);
+                charCount--;
+            }
+
+            charCount++;
+
+            line = text.slice(prevCount, charCount);
+            altText = altText.concat(line, '\n');
+
+            prevCount = charCount;
+            charCount += lineLen;
+
+            console.log(`\nCurrent-Line: ("${line}")\n`);
+            console.log(`\nAltered-Text: "${altText}"\n`);
+            console.log(`\nCharCount: ${charCount}`);
+            console.log(`\nPrevCount: ${prevCount}`);
+        }
     }
 }
 
-module.exports.PlainTextDocument = PlainTextDocument;
+module.exports.TextDocument = TextDocument;
