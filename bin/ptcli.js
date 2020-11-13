a923bde7318e97653b33872210848d841b0abe37
Katana-Development <katana.development@gmail.com>
2020-11-13 01:52:50 -0800

    Finished TextDocument class for now. I am very Happy with it so far.

--- bin/ptcli.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-/** @format */
-
-const chalk = require('chalk');
-const command = process.argv;
-const { MSG_INITITAL } = require('../lib/constants.json').CLI;
-
-console.info();
--- index.js
+++ index.js
@@ -1,133 +1,51 @@
 /**
  *  @prettier
- *  @name PlainTextInterface
- *  @license MIT
- *  @publicd
- *  @summary https://github.com/Katana-Development/.git
+ *  @name PLAIN_TEXT_INTERFACE
  *  @author Andrew J Chambers
- *  @file"./index.js"
- *  @fileoverview
+ *  @file ./index.js
+ *  @fileoverview "The Main-File of Module PlainTextInterface."
+ *  @summary https://github.com/Katana-Development/PlainTextInterface.git
+ *  @license MIT
  **/
-
-/*
-   
-  -->                                <|[TODO LIST]|>                                <--
-  ||> --------------------------------------^-------------------------------------- <||
-   <=<( TODO CREATE )>=> "Create a new error class for FileType Errorz." `
-   
-   <=<( TODO CREATE )>=> "Create the following methods addText(text), appendText(text), saveFile(), rmFile() and changeFilepath(newFilepath) "
-   
-   <=<( TODO CREATE )>=> "Test the maxStorage RangeErrors, and proof-read the their messages";
-
-
-
-
-
-*/
 
 'strict';
 
 const fs = require('fs');
-const { extname } = require('path');
-const { O_CREAT, O_RDWR } = fs.constants;
-const { Buffer } = require('buffer');
+const { O_CREAT, O_RDWR, O_TRUNC, O_WRONLY } = fs.constants;
 
-const {
-   E_MSG_FPATH_FTYP,
-   E_MSG_FPATH_TYP,
-   E_MSG_RNG_MAX,
-   E_MSG_RNG_MIN
-} = require('./constants.json').errorMessages;
-const {
-   TXTDOC_MAX_BYTES,
-   TXTDOC_MIN_BYTES
-} = require('./constants.json').numericConstants;
-
-/**
- * @name TextDocument
- * @classdesc Creates an abstraction of a plain-text file, and implements an interface to handle the files data, handle the file as data and to alter or manipulate the file.
- **/
 class TextDocument {
    #fpath;
-   #buff;
-   #fd;
-   #stat;
    #text;
 
-   constructor(fpath = null, maxStorage = 1000000) {
-      const bytes = maxStorage;
+   constructor(fpath) {
       this.#fpath = fpath;
 
-      if (typeof this.#fpath != 'string') {
-         throw new TypeError(E_MSG_FPATH_TYP);
-      }
+      var fsize = fs.statSync(this.#fpath).size;
+      var fd = fs.openSync(fpath, O_CREAT | O_RDWR);
+      var buf = Buffer.allocUnsafe(fsize);
 
-      if (extname(this.#fpath) != '.txt') {
-         throw new Error(E_MSG_FPATH_FTYP);
-      }
+      fs.readSync(fd, buf, { offset: 0, length: fsize, position: 0 });
+      fs.closeSync(fd);
 
-      if (bytes > TXTDOC_MAX_BYTES) {
-         throw new RangeError(E_MSG_RNG_MAX);
-      } else if (bytes < TXTDOC_MIN_BYTES) {
-         bytes = TXTDOC_MIN_BYTES;
-      }
-
-      this.#fd = fs.openSync(this.#fpath);
-      this.#stat = fs.statSync(this.#fpath);
-      this.#buff = Buffer.alloc(1000000);
-
-      fs.chmodSync(fpath, 0o664);
-      fs.readSync(this.#fd, this.#buff, { length: this.#stat.size });
-
-      this.text = this.#buff.toString('utf-8');
+      this.text = buf.toString('utf8', 0, fsize);
    }
 
-   get text() {
-      return this.#text;
+   save() {
+      var buf = Buffer.from(this.text);
+      var fd = fs.openSync(this.#fpath, O_TRUNC | O_WRONLY);
+      var result = fs.writeSync(fd, buf, 0, this.text.length, 0);
+
+      fs.closeSync(fd);
+
+      return result;
    }
 
    set text(text) {
       this.#text = text;
    }
 
-   setLineLength(maxCharsPerLine = 80) {
-      const text = this.text;
-      const lineLen = maxCharsPerLine;
-
-      let altText = '';
-      let line = '';
-      let prevCount = 0;
-      let charCount = lineLen;
-
-      while (charCount <= text.length) {
-         while (text.charAt(charCount) != ' ') {
-            charCount--;
-         }
-
-         charCount++;
-         line = text.slice(prevCount, charCount);
-         altText = altText.concat(line, '\n');
-
-         prevCount = charCount;
-         charCount += lineLen;
-
-         console.log(`\nCurrent-Line: ("${line}")\n`);
-         console.log(`\nAltered-Text: \n${altText}`);
-         console.log(`\nCharCount: ${charCount}`);
-         console.log(`\nPrevCount: ${prevCount}`);
-         console.log(`\nOrginal Text Length: ${text.length}`);
-      }
-
-      var lastLineLen = text.length - charCount + lineLen;
-
-      console.log(`\nLast Line Length: ${lastLineLen}`);
-
-      var lastLine = text.slice(prevCount, prevCount + lastLineLen);
-      console.log(`\nLast Line: ${lastLine}`);
-      altText = altText.concat(lastLine);
-      console.log(altText);
-
-      return altText;
+   get text() {
+      return this.#text;
    }
 }
 
